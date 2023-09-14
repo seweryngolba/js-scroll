aa4d71c9f0d4a264c03699ff2e245227306df966
Seweryn Golba <sgoolb@gmail.com>
2023-09-14 22:35:33 +0200

    opacity and move

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 $(document).on("scroll", function () {
-  $("h1").css("left", Math.max(1400 - 0.35 * window.scrollY, 10) + "px");
+  $("h1").css({
+    right: Math.max(550 - 0.35 * window.scrollY, -1400) + "px",
+    opacity: Math.max(1 - window.scrollY / 1400, 0),
+  });
 });
