4135a3efe75578cb7c0862c65f203bae620eec88
Seweryn Golba <sgoolb@gmail.com>
2023-09-15 23:42:53 +0200

    add js

--- scroll2/02-index.js
+++ scroll2/02-index.js
@@ -1,5 +1,14 @@
 $(document).on("scroll", function () {
+  let scrollY = window.scrollY;
+  let fontSize;
+
+  if (scrollY <= 320) {
+    fontSize = 16 + scrollY / 10;
+  } else {
+    fontSize = 32 + Math.log(scrollY - 320) * 10;
+  }
+
   $("h1").css({
-    fontSize: Math.max(16 + 0.35 * window.scrollY, 16) + "px",
+    fontSize: Math.min(fontSize, 120) + "px",
   });
 });
